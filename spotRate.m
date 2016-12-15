c65eab4ce8c369d1ea55ad4dd7889b0818d03c87
Peter Szedejesi <petsz050@student.liu.se>
2016-12-15 15:44:32 +0100

    Added priceOIS

--- spotRate.m
+++ spotRate.m
@@ -17,18 +17,3 @@ end
 r = r / t;
 
 end
-
-
-
-% F�r att ist�llet integrera �ver varje segment i tMat:
-% for i = 1:find(tMat == tSpot);
-%     
-%     if any(tSpline == tMat(i)) == 1 % if tMat(i) exists in tSpline use that one
-%         s = find(tSpline == min(tSpline(tSpline == tMat(i)))); %if this exists in tSpline set equality 
-%     else % else use the smallest one of the above
-%         s = find(tSpline == min(tSpline(tSpline > tMat(i)))); %if it does not exists in tSpline set inequality 
-%     end
-%     
-%     r = r + (1/4)*coeffVec(s)*(tMat(i+1) - tMat(i))^4 + (1/3)*coeffVec(s+1)*(tMat(i+1) - tMat(i))^3 + (1/2)*coeffVec(s+2)*(tMat(i+1) - tMat(i))^2 + coeffVec(s+3)*(tMat(i+1) - tMat(i));
-%     
-% end
