b771e7265444f5b94521fb5da992ba3d6d1081e3
Qjornt <ado.berberovic@gmail.com>
2016-12-15 14:49:59 +0100

    made price data into a .mat file which loads mid prices using getMidPrices.m

---
+++ getMidPrices.m
@@ -0,0 +1,8 @@
+function [ midPrices ] = getMidPrices()
+% Fetches mid data from observed historical prices for OIS with maturities
+% 1/52, 1/12, 2/12, 3/12, 6/12, 9/12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12,
+% 15, 20, 25 and 30 years.
+    load dataPrices.mat
+    midPrices = (dataask(2:end,2:end) + databid(2:end,2:end))./2;
+end
+
