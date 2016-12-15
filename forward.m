52e6ccfa0f626525c67aa98dd48ee051bdbf4c54
Jesper Otterholm <jesot351@student.liu.se>
2016-12-15 09:42:25 +0100

    Some forward rates calculation reorganization.

--- forward.m
+++ forward.m
@@ -10,12 +10,23 @@ F = eye(m);
 b_e = zeros(m,1); % Actual prices
 
 f = zeros(4*n,1); % Parameters for each spline
-f_bar = zeros(size(f)); % Permutated initial guess (f_tilde)
+f_tilde = zeros(size(f)); % Initial guess
+f_bar = P*f_tilde; % Permutated initial guess (f_tilde)
 x = zeros(size(f)); % Permutated perturbations (delta_f)
 x_B = zeros(3*(n-1),1); % Basic variables
 x_N = zeros(n+3,1); % Non-basic variables
 f_bar_B = f_bar(1:length(x_B));
 f_bar_N = f_bar(length(x_B)+1:end);
+
+B_B = -eye(size(x_B,1));
+B_N = zeros(size(x_B,1),size(x_N,1));
+b = zeros(size(x_B)); % R.h.s for spline conditions
+
+H = P*regul*P';
+H_BB = H(1:length(x_B),1:length(x_B)); 
+H_BN = H(1:length(x_B),length(x_B)+1:end);
+H_NN = H(length(x_B)+1:end,length(x_B)+1:end);
+H_NB = H(length(x_B)+1:end,1:length(x_B));
 
 % Re-compute every iteration
 g = zeros(m,1); % Takes forward interest to ois
@@ -28,26 +39,24 @@ A_B = -F\grad_g_bar_B';
 A_N = -F\grad_g_bar_N';
 a = -F\(g-b_e);
 
-B_B = -eye(size(x_B,1));
-B_N = zeros(size(x_B,1),size(x_N,1));
-b = zeros(size(x_B)); % R.h.s for spline conditions
-
-H = P*regul*P';
-H_BB = H(1:length(x_B),1:length(x_B)); 
-H_BN = H(1:length(x_B),length(x_B)+1:end);
-H_NN = H(length(x_B)+1:end,length(x_B)+1:end);
-H_NB = H(length(x_B)+1:end,1:length(x_B));
-
 H_tilde = (B_B\B_N)'*H_BB*(B_B\B_N) - 2*(B_B\B_N)'*H_BN + H_NN + ...
           (A_N - A_B*(B_B\B_N))'*E*(A_N - A_B*(B_B\B_N));
-% Assume b = 0
+% Assuming b = 0
 h_tilde = ((-a)'*E*(A_N-A_B*(B_B\B_N)) + f_bar_B'*(H_BN - H_BB*(B_B\B_N)) + ...
           f_bar_N'*(H_NN - H_NB*(B_B\B_N)))';
-      
+
+% Calculate solution
 x_N = -H_tilde\h_tilde;
 x_B = -(B_B\B_N)*x_N;
 
-f = P'*([f_bar_B; f_bar_N] + [x_B; x_N]);
+% Update state
+f_tilde = f_tilde + P'*[x_B ; x_N];
+f_bar = P*f_tilde;
+f_bar_B = f_bar(1:length(x_B));
+f_bar_N = f_bar(length(x_B)+1:end);
+
+% Final result
+f = f_tilde;
 
 
 
